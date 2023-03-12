196345e095c04469c676872e3714ce6323b53a57
Xenia Golbraykh <karzerin@gmail.com>
2023-03-12 18:04:32 +0300

    Создала программу по решению

---
+++ FinWork/Program.cs
@@ -0,0 +1,23 @@
+﻿string[] array = new string[5] { "Wow", "the", "world", "is", "mine" };
+string[] newArray = new string[array.Length];
+
+void FindWordInArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[count] = array[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    Console.WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(" ", newArray)}]");
+
+}
+Console.Clear();
+FindWordInArray(array, newArray);
+PrintArray(array);
